c56afb01820ed2e77810840199a702c232a775eb
Vladimir Filipovic <vladofilipovic@hotmail.com>
2022-10-10 13:48:03 +0200

    popravio

--- src/rs/math/oop/g02/p01/klaseObjekti/StudentPokretanje.java
+++ src/rs/math/oop/g02/p01/klaseObjekti/StudentPokretanje.java
@@ -4,13 +4,13 @@ class StudentPokretanje {
     // улазна тачка програма
     public static void main(String[] args) {
         Student prvi = new Student();
-        prvi.ime = "Марко Илић";
+        prvi.ime = "Marko Markovic";
         prvi.brojIndeksa = 243;
         prvi.naBudzetu = false;
 
         Student drugi;
         drugi = new Student();
-        drugi.ime = "Ђина Сарић";
+        drugi.ime = "Ivana Ivanovic";
         drugi.brojIndeksa = 25;
         drugi.naBudzetu = true;
 
